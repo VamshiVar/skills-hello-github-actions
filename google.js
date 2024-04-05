1dc3fd42265829c6edf9a2eef93c7db1a0be37c1
Sai Vamshi <90269703+VamshiVar@users.noreply.github.com>
2024-04-05 16:10:52 +0530

    Update google.js

--- google.js
+++ google.js
@@ -1,1 +1,12 @@
-window.open("https://www.google.com", "_blank");
+const axios = require('axios');
+
+async function fetchResponse() {
+  try {
+    const response = await axios.get('https://www.google.com');
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error in fetaching response:", error)
+  }
+}
+
+fetchResponse();
